894e0898a413bfca1b762aaf38b5708b90f05dc6
David Zbarsky <dzbarsky@gmail.com>
2023-09-28 17:14:38 -0400

    Disconnect from tailscail on shutdown so we can reuse service name

--- start.sh
+++ start.sh
@@ -2,4 +2,16 @@
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=monica
-/usr/local/bin/entrypoint.sh apache2-foreground
+
+cleanup() {
+    echo "Received SIGINT. Cleaning up..."
+    /app/tailscale logout
+    kill $apache_pid
+    exit
+}
+
+trap cleanup INT TERM
+
+/usr/local/bin/entrypoint.sh apache2-foreground &
+apache_pid=$!
+wait $apache_pid
